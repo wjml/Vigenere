697d78e24d4bea0fbc1dee279f85fc0a9221a533
willian <willian.lourenco@novaandradina.org>
2019-06-28 17:29:17 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+// faz o express procurar na pasta 'public' pelos assets (css/js/img)
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res) {
+	res.sendfile("./index.html");
+});
+
+app.listen(port, function() {
+	console.log("Our app is running on http://localhost:" + port);
+});
