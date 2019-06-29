2910eaf1567e71a92162d21e65d5d868255311b6
willian <willian.lourenco@novaandradina.org>
2019-06-29 11:49:13 -0400

    .

--- public/vigenere.js
+++ public/vigenere.js
@@ -1,204 +1,46 @@
-// Alfabeto
-var alfabeto = "abcdefghijklmnopqrstuvwxyz";
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+const strAccent = "áàãâäéèêëíìîïóòõôöúùûüçÁÀÃÂÄÉÈÊËÍÌÎÏÓÒÕÖÔÚÙÛÜÇ";
+const strNoAccent = "aaaaaeeeeiiiiooooouuuucAAAAAEEEEIIIIOOOOOUUUUC";
+rtn = "";
+var encrypt = document.getElementById("encrypt");
+var decrypt = document.getElementById("decrypt");
+var key = document.getElementById("key");
+var input = document.getElementById("input");
+var output = document.getElementById("output");
 
-$("#encrypt").click(function() {
-	var key = $("#key").val();
-	key = key.toLowerCase();
-
-	var input = $("#input").val();
-	input = input.toLowerCase();
-
-	var output = $("#output");
-
-	key = alterar_chave(key, input);
-
-	output.text(cifrar(input, key));
-});
-
-$("#decrypt").click(function() {
-	var key = $("#key").val();
-	key = key.toLowerCase();
-
-	var input = $("#input").val();
-	input = input.toLowerCase();
-
-	var output = $("#output");
-
-	key = alterar_chave(key, input);
-
-	output.text(decifrar(input, key));
-});
-// Cifrar palavra
-var cifrar_letra = function(letra, valor) {
-	for (var i = 0; i < letra.length; i++) {
-		for (var j = 0; j < alfabeto.length; j++) {
-			if (alfabeto[j] == letra[i]) {
-				return alfabeto[(j + valor) % 26];
+function first() {
+	rtn = "";
+	j = 0;
+	regex = [a - zA - Z] + g;
+	for (i = 0; i < input.value.length; i++) {
+		console.log(!input.value[i] == regex);
+		if (input.value[i] == /[a-z]/i || strAccent.includes(input.value[i])) {
+			if (j <= key.value.length) {
+				rtn += key.value[j++];
+			} else {
+				j = 0;
 			}
-		}
-	}
-};
-
-// Decifrar palavra
-var decifrar_letra = function(letra, valor) {
-	for (var i = 0; i < letra.length; i++) {
-		for (var j = 0; j < alfabeto.length; j += 1) {
-			if (letra[i] == alfabeto[j]) {
-				return alfabeto[(j - valor + 26) % 26];
-			}
-		}
-	}
-};
-
-// Retornar posicao no alfabeto
-var posicao = function(caracter) {
-	for (var i = 0; i < alfabeto.length; i += 1) {
-		if (alfabeto[i] == caracter) {
-			return i;
-		}
-	}
-};
-
-// Cifrar texto
-var cifrar = function(texto, chave) {
-	var cifrado = "";
-
-	texto = remover_acento(texto);
-
-	for (var i = 0; i < texto.length; i += 1) {
-		switch (texto[i]) {
-			case " ":
-				cifrado += texto[i];
-				break;
-			case ",":
-				cifrado += texto[i];
-				break;
-			case ".":
-				cifrado += texto[i];
-				break;
-			case "!":
-				cifrado += texto[i];
-				break;
-			case "?":
-				cifrado += texto[i];
-				break;
-			case '"':
-				cifrado += texto[i];
-				break;
-			default:
-				cifrado += cifrar_letra(texto[i], posicao(chave[i]));
-		}
-	}
-
-	return cifrado;
-};
-
-// Decifrar texto
-var decifrar = function(texto, chave) {
-	var decifrado = "";
-
-	texto = remover_acento(texto);
-
-	for (var i = 0; i < texto.length; i += 1) {
-		switch (texto[i]) {
-			case " ":
-				decifrado += texto[i];
-				break;
-			case ",":
-				decifrado += texto[i];
-				break;
-			case ".":
-				decifrado += texto[i];
-				break;
-			case "!":
-				decifrado += texto[i];
-				break;
-			case "?":
-				decifrado += texto[i];
-				break;
-			case '"':
-				decifrado += texto[i];
-				break;
-			default:
-				decifrado += decifrar_letra(texto[i], posicao(chave[i]));
-		}
-	}
-
-	return decifrado;
-};
-
-// Alterar chave
-var alterar_chave = function(chave, texto) {
-	var nova_chave = "";
-
-	chave = retirar_espacos(chave);
-	chave = remover_acento(chave);
-
-	while (nova_chave.length < texto.length) {
-		nova_chave += chave;
-	}
-
-	if (nova_chave.length > texto.length) {
-		chave = "";
-
-		for (var i = 0; i < texto.length; i += 1) {
-			chave += nova_chave[i];
-		}
-
-		console.log(nova_chave);
-
-		return adicionar_espacos(texto, chave);
-	}
-
-	return adicionar_espacos(texto, nova_chave);
-};
-
-// Retira espaços da chave
-var retirar_espacos = function(chave) {
-	var nova_chave = "";
-
-	for (var i = 0; i < chave.length; i++) {
-		if (chave[i] != " ") {
-			nova_chave += chave[i];
-		}
-	}
-
-	return nova_chave;
-};
-
-// Adiciona espaços na chave de acordo com o texto
-var adicionar_espacos = function(texto, chave) {
-	var nova_chave = "";
-	var j = 0;
-
-	for (var i = 0; i < texto.length; i++) {
-		if (texto[i] == " ") {
-			nova_chave += " ";
 		} else {
-			nova_chave += chave[j];
-			j++;
+			rtn += " ";
 		}
 	}
+}
 
-	console.log(nova_chave);
-
-	return nova_chave;
-};
-
-// Retirar acentuação
-function remover_acento(texto) {
-	str_acento = "áàãâäéèêëíìîïóòõôöúùûüçÁÀÃÂÄÉÈÊËÍÌÎÏÓÒÕÖÔÚÙÛÜÇ";
-	str_sem_acento = "aaaaaeeeeiiiiooooouuuucAAAAAEEEEIIIIOOOOOUUUUC";
-
-	var nova = "";
-
-	for (var i = 0; i < texto.length; i++) {
-		if (str_acento.indexOf(texto.charAt(i)) != -1) {
-			nova += str_sem_acento.substr(str_acento.search(texto.substr(i, 1)), 1);
+function second(mode) {
+	first();
+	//mode==true: encrypt
+	//mode==false: decrypt
+	mode ? (mode = 1) : (mode = -1);
+	output.value = "";
+	for (i = 0; i < input.value.length; i++) {
+		if (!rtn[i] == " ") {
+			output.value +=
+				alphabet[
+					(alphabet.indexOf(input.value[i]) + alphabet.indexOf(rtn[i]) * mode) %
+						25
+				];
 		} else {
-			nova += texto.substr(i, 1);
+			output.value += input.value[i];
 		}
 	}
-
-	return nova;
 }
